5e11a256636ece7fe441f6dfda3d3b279808b57f
Iammohitwalia <mohit.strixdevelopment@gmail.com>
2022-05-13 10:11:23 +0530

    Updates

---
+++ precache-manifest.6b35cafd4fdf1fcc1560dc14a7c8f3a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b342dfc2d5d9070b17a1503b51b953f3",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "66cafadf59b6e156b101",
+    "url": "/Portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "5a3bd1a065057ccc22fe",
+    "url": "/Portfolio/static/css/main.abb251cb.chunk.css"
+  },
+  {
+    "revision": "66cafadf59b6e156b101",
+    "url": "/Portfolio/static/js/2.66ea928c.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/Portfolio/static/js/2.66ea928c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a3bd1a065057ccc22fe",
+    "url": "/Portfolio/static/js/main.92a8734b.chunk.js"
+  },
+  {
+    "revision": "c125469937b460e6de34",
+    "url": "/Portfolio/static/js/runtime-main.3379861c.js"
+  }
+]);
